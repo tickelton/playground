943e493c22e454a02ff967a3102fd24399615bab
tickelton <tickelton@gmail.com>
2021-06-18 21:40:30 +0200

    Adds scrape-o-mat skeleton

--- scrape-o-mat/scrape-o-mat.py
+++ scrape-o-mat/scrape-o-mat.py
@@ -6,6 +6,10 @@
 import sys
 import importlib
 import argparse
+import http.cookiejar
+import urllib.parse
+import urllib.request
+#import pandas
 
 PROGNAME="scrape-o-mat"
 VERSION="0.0.1"
@@ -18,9 +22,52 @@ def main():
                                 help='configuration module name')
     args = parser.parse_args()
 
-    cfg = importlib.import_module(args.config_module[0])
+    cfg = importlib.import_module(args.config_module[0]).cfg
 
-    print(cfg.c)
+    print(cfg)
+
+    cookie_jar = http.cookiejar.CookieJar()
+    op = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
+    post_data = urllib.parse.urlencode(cfg.login.post_data)
+    post_data = post_data.encode('ascii')
+    op.addheaders = [('User-Agent', cfg.user_agent)]
+    try:
+        result = op.open(cfg.login.url, post_data)
+        status = result.getcode()
+        reason = result.msg
+    except HTTPError as e:
+        status = e.code
+        reason = e.msg
+    except OSError as e:
+        print(str(e))
+        raise
+
+    if status == 200:
+       res.read()
+    else:
+        print("foofail")
+        raise DistutilsError(msg)
+
+    res.close()
+
+
+    # insert random delay to appear more human
+
+    post_data = urllib.parse.urlencode(cfg.fetch.post_data)
+    post_data = post_data.encode('ascii')
+    res = op.open(cfg.fetch.url, post_data)
+    res.getcode()
+    res.read()
+    foo = res.read()
+    dfs = pandas.read_html(foo)
+
+    # insert more random delay
+
+    res = op.open(cfg.logout.url)
+    res.getcode()
+    res.read()
+
+
 
 if __name__ == "__main__":
     main()
