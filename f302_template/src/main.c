95302f0bb9bdc10e70492c218256167232cf0a41
tickelton <tickelton@gmail.com>
2021-06-19 22:21:53 +0200

    Adds STM32F3 template project
    NOTE: The project compiles and runs but HAL_Delay doesn't
    work. So don't blindly copy that!

---
+++ f302_template/src/main.c
@@ -0,0 +1,102 @@
+
+#include <stm32f3xx_hal.h>
+#include <stm32f3xx_nucleo.h>
+//#include <stm32f3xx_nucleo_144.h>
+#include "stm32f3xx_hal_conf.h"
+
+static GPIO_InitTypeDef  GPIO_InitStruct;
+
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+int main(void)
+{
+/*
+	BSP_LED_Init(LED1);
+	BSP_LED_Init(LED2);
+	BSP_LED_Init(LED3);
+
+	for(;;)
+	{
+		BSP_LED_Toggle(LED1);
+		for (int j = 0; j < 100000; j++)
+			;
+
+		BSP_LED_Toggle(LED2);
+		for (int j = 0; j < 100000; j++)
+			;
+
+		BSP_LED_Toggle(LED3);
+		for (int j = 0; j < 100000; j++)
+			;
+	}
+*/
+  HAL_Init();
+
+  /* Configure the system clock to 64 MHz */
+  SystemClock_Config();
+
+  /* -1- Enable each GPIO Clock (to be able to program the configuration registers) */
+  LED2_GPIO_CLK_ENABLE();
+
+  /* -2- Configure IOs in output push-pull mode to drive external LEDs */
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+  GPIO_InitStruct.Pin = LED2_PIN;
+  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+
+
+  /* -3- Toggle IOs in an infinite loop */
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+    /* Insert delay 100 ms */
+    HAL_Delay(1000);
+  }
+
+	return 0;
+}
+
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  
+  /* HSI Oscillator already ON after system reset, activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  /* User may add here some code to deal with this error */
+  while(1)
+  {
+  }
+}
+
