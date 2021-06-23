73427531488a42c05a1d986199c3b20ee80dad06
tickelton <tickelton@gmail.com>
2021-06-23 21:53:03 +0200

    Adds working implementation of scrape-o-mat

--- scrape-o-mat/config_dummy.py
+++ scrape-o-mat/config_dummy.py
@@ -1,4 +1,30 @@
-c = {
-    'foo' : 'bar',
-    'baz' : 3,
-    }
+configuration = {
+    "user_agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+    "login" : {
+        "url" : "https://example.com/Admin/Login",
+        "post_data" : {
+            "Email" : "foobar@example.com",
+            "Password" : "foobar",
+            "mode" : "Light"
+        }
+    },
+    "fetch" : {
+        "url" : "https://example.com/Data/Select",
+        "post_data" : {
+            "Min" : 500,
+            "Max" : 512,
+            "type" : "basic"
+        }
+    },
+    "logout" : {
+        "url" : "https://example.com/Admin/Logout",
+    },
+    "target_frame" : 3,
+    "expected_cols" : [
+        'Value (ms)',
+        'Date',
+        'Foobar',
+    ],
+    "db_path" : "exampledb.sqlite",
+    "table_name" : "MyData"
+}
--- scrape-o-mat/scrape-o-mat.py
+++ scrape-o-mat/scrape-o-mat.py
@@ -6,14 +6,68 @@
 import sys
 import importlib
 import argparse
+import random
+import time
+import pandas
+import sqlite3
 import http.cookiejar
 import urllib.parse
 import urllib.request
-#import pandas
+from datetime import datetime
 
 PROGNAME="scrape-o-mat"
 VERSION="0.0.1"
+WAIT_S_MIN = 15
+WAIT_S_MAX = 45
 
+def do_random_wait():
+    wait_time = random.randint(WAIT_S_MIN, WAIT_S_MAX);
+    print("Wating for {} seconds.".format(wait_time))
+    time.sleep(wait_time)
+
+def do_post_request(op, cfg, step):
+
+    post_data = urllib.parse.urlencode(cfg[step]["post_data"])
+    post_data = post_data.encode('ascii')
+    op.addheaders = [('User-Agent', cfg["user_agent"])]
+    rc = 0
+    try:
+        res = op.open(cfg[step]["url"], post_data)
+        rc = res.getcode()
+    except urllib.request.HTTPError as e:
+        rc = e.code
+        print(str(e))
+    except OSError as e:
+        print(str(e))
+
+    data = None
+    if rc == 200:
+       data = res.read()
+
+    res.close()
+
+    return (rc, data)
+
+def do_get_request(op, cfg, step):
+
+    op.addheaders = [('User-Agent', cfg["user_agent"])]
+    rc = 0
+    try:
+        res = op.open(cfg[step]["url"])
+        rc = res.getcode()
+    except urllib.request.HTTPError as e:
+        rc = e.code
+        print(str(e))
+    except OSError as e:
+        print(str(e))
+
+    data = None
+    if rc == 200:
+       data = res.read()
+
+    res.close()
+
+    return (rc, data)
 
 def main():
     parser = argparse.ArgumentParser(prog=PROGNAME)
@@ -22,52 +76,77 @@ def main():
                                 help='configuration module name')
     args = parser.parse_args()
 
-    cfg = importlib.import_module(args.config_module[0]).cfg
+    cfg = importlib.import_module(args.config_module[0]).configuration
 
-    print(cfg)
+    if not "fetch" in cfg or not "url" in cfg["fetch"]:
+        print("Error: config data is missing 'fetch' URL. Aborting.")
+        sys.exit(1)
 
     cookie_jar = http.cookiejar.CookieJar()
     op = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
-    post_data = urllib.parse.urlencode(cfg.login.post_data)
-    post_data = post_data.encode('ascii')
-    op.addheaders = [('User-Agent', cfg.user_agent)]
-    try:
-        result = op.open(cfg.login.url, post_data)
-        status = result.getcode()
-        reason = result.msg
-    except HTTPError as e:
-        status = e.code
-        reason = e.msg
-    except OSError as e:
-        print(str(e))
-        raise
 
-    if status == 200:
-       res.read()
-    else:
-        print("foofail")
-        raise DistutilsError(msg)
+    # execute login
+    if "login" in cfg:
+        if "post_data" in cfg["login"]:
+            (rc, data) = do_post_request(op, cfg, "login")
+            if rc == 200:
+                print("Login successful.")
+            else:
+                print("Login failure!")
+                op.close()
+                sys.exit(1)
 
-    res.close()
+    # wait for a random number of seconds to appear more human
+    do_random_wait()
 
+    # fetch data
+    if "post_data" in cfg["fetch"]:
+        (rc, data) = do_post_request(op, cfg, "fetch")
+        if rc == 200:
+            print("Fetch successful.")
+            dfs = pandas.read_html(data)
+        else:
+            print("Fetch failure!")
+            op.close()
+            sys.exit(1)
 
-    # insert random delay to appear more human
+    # more random delay
+    do_random_wait()
 
-    post_data = urllib.parse.urlencode(cfg.fetch.post_data)
-    post_data = post_data.encode('ascii')
-    res = op.open(cfg.fetch.url, post_data)
-    res.getcode()
-    res.read()
-    foo = res.read()
-    dfs = pandas.read_html(foo)
+    # execute logout
+    if "logout" in cfg:
+        if "post_data" in cfg["logout"]:
+            (rc, data) = do_post_request(op, cfg, "logout")
+        else:
+            (rc, data) = do_get_request(op, cfg, "logout")
+        if rc == 200:
+            print("Logout successful.")
+        else:
+            print("Logout failure!")
+            op.close()
+            sys.exit(1)
 
-    # insert more random delay
+    # select target data fram
+    df = dfs[cfg["target_frame"]]
 
-    res = op.open(cfg.logout.url)
-    res.getcode()
-    res.read()
+    # validate dataset
+    if not set(cfg["expected_cols"]) == set(df.keys()):
+        print("Error: data does not match expected format!")
+        print("Expected: {}".format(set(cfg["expected_cols"])))
+        print("Got: {}".format(set(df.keys())))
+        sys.exit(1)
 
+    # add timestap to data set
+    df['date'] = [ datetime.now() for i in range(len(df)) ]
 
+    # save to database
+    print("Saving...")
+    db = "test.sqlite"
+    conn = sqlite3.connect(cfg["db_path"])
+    df.to_sql(name=cfg["table_name"], con=conn, if_exists='append')
+    conn.close()
+
+    print("Finished.")
 
 if __name__ == "__main__":
     main()
