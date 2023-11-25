3d4807696c44369f97e92037ae4553f2a1cf6b8d
tickelton <tickelton@gmail.com>
2023-11-25 10:59:06 +0100

    Adds 30DoC:28

---
+++ challenges/hr/30_days_of_code/day_28-regex_patterns_and_intro_to_databses.cpp
@@ -0,0 +1,127 @@
+/*
+# RegEx, Patterns, and Intro to Databases
+
+## Objective
+Today, we're working with regular expressions.
+
+## Task
+Consider a database table, Emails, which has the attributes First Name and Email ID. Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people whose email address ends in @gmail.com.
+
+## Input Format
+
+The first line contains an integer, N, total number of rows in the table.
+Each of the N subsequent lines contains 2 space-separated strings denoting a person's first name and email ID, respectively.
+
+## Constraints
+
+* 2<= N <= 30
+* Each of the first names consists of lower case letters [a-z] only.
+* Each of the email IDs consists of lower case letters [a-z], @ and . only.
+* The length of the first name is no longer than 20.
+* The length of the email ID is no longer than 50.
+
+## Output Format
+
+Print an alphabetically-ordered list of first names for every user with a gmail account. Each name must be printed on a new line.
+
+## Sample Input
+
+6
+riya riya@gmail.com
+julia julia@julia.me
+julia sjulia@gmail.com
+julia julia@gmail.com
+samantha samantha@gmail.com
+tanya tanya@gmail.com
+
+## Sample Output
+
+julia
+julia
+riya
+samantha
+tanya
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+int main()
+{
+    std::regex gmail_regex("[a-z]+@gmail.com");
+    vector<string> targets;
+
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+    
+    for (int N_itr = 0; N_itr < N; N_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        string firstName = first_multiple_input[0];
+
+        string emailID = first_multiple_input[1];
+        
+        if (regex_search(emailID, gmail_regex)) {
+            targets.push_back(firstName);
+        }
+    }
+
+    sort(targets.begin(), targets.end());
+    for (auto s: targets) {
+        cout << s << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
