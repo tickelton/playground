83ce3c6d87ab1551aa43df37692dab80590facc0
tickelton <tickelton@gmail.com>
2023-12-25 12:05:27 +0100

    Adds lc:decode_ways

---
+++ challenges/lc/dp/decode_ways.cpp
@@ -0,0 +1,104 @@
+/*
+
+# Decode Ways
+
+A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+
+To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+    "AAJF" with the grouping (1 1 10 6)
+    "KJF" with the grouping (11 10 6)
+
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+Given a string s containing only digits, return the number of ways to decode it.
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+
+## Example 1:
+
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+
+
+## Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+
+## Example 3:
+
+Input: s = "06"
+Output: 0
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+
+
+## Constraints:
+
+* 1 <= s.length <= 100
+* s contains only digits and may contain leading zero(s).
+
+*/
+
+class Solution {
+public:
+    set<string> mappings = { "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                    "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                    "20", "21", "22", "23", "24", "25", "26" };
+    bool isValidMapping(string s) {
+        if (mappings.find(s) != mappings.end()) {
+            return true;
+        }
+        return false;
+    }
+    int numDecodingsFrom(int idx, string& s) {
+        if (idx >= s.size()) { return 0; }
+        
+        string candidate;
+        candidate.push_back(s[idx]);
+        bool validSingle = isValidMapping(candidate);
+        if (idx == s.size()-1) { return validSingle; }
+
+        int decodings = 0;
+        if (validSingle) {
+            int tmp = numDecodingsFrom(idx+1, s);
+            if (tmp>0) {
+                decodings += tmp;
+            }
+        }
+
+        candidate.push_back(s[idx+1]);
+        bool validDouble = isValidMapping(candidate);
+        if (idx == s.size()-2) { 
+            if (validSingle && validDouble && decodings>0) {
+                return 2;
+            } else if (validSingle && decodings>0) {
+                return 1;
+            } else if (validDouble) {
+                return 1;
+            } else {
+                return 0;
+            }
+         }
+
+        if (validDouble) {
+            int tmp = numDecodingsFrom(idx+2, s);
+            if (tmp>0) {
+                decodings += tmp;
+            }
+        }
+        return decodings;
+    }
+    int numDecodings(string s) {
+        return numDecodingsFrom(0, s);
+    }
+};
