ec399a8a462871d9902bf55cd6132f3b38a213f6
tickelton <tickelton@gmail.com>
2024-01-01 13:02:28 +0100

    Adds another solution for lc:minimum_difficulty_of_a_job_schedule

--- challenges/lc/dp/minimum_difficulty_of_a_job_schedule.cpp
+++ challenges/lc/dp/minimum_difficulty_of_a_job_schedule.cpp
@@ -1,10 +1,6 @@
 /*
 
-Minimum Difficulty of a Job Schedule
-Hard
-2.9K
-273
-Companies
+# Minimum Difficulty of a Job Schedule
 
 You want to schedule a list of jobs in d days. Jobs are dependent (i.e To work on the ith job, you have to finish all the jobs j where 0 <= j < i).
 
@@ -14,41 +10,41 @@ You are given an integer array jobDifficulty and an integer d. The difficulty of
 
 Return the minimum difficulty of a job schedule. If you cannot find a schedule for the jobs return -1.
 
- 
 
-Example 1:
+## Example 1:
 
 Input: jobDifficulty = [6,5,4,3,2,1], d = 2
 Output: 7
 Explanation: First day you can finish the first 5 jobs, total difficulty = 6.
 Second day you can finish the last job, total difficulty = 1.
-The difficulty of the schedule = 6 + 1 = 7 
+The difficulty of the schedule = 6 + 1 = 7
 
-Example 2:
+
+## Example 2:
 
 Input: jobDifficulty = [9,9,9], d = 4
 Output: -1
 Explanation: If you finish a job per day you will still have a free day. you cannot find a schedule for the given jobs.
 
-Example 3:
+
+## Example 3:
 
 Input: jobDifficulty = [1,1,1], d = 3
 Output: 3
 Explanation: The schedule is one job per day. total difficulty will be 3.
 
- 
 
-Constraints:
+## Constraints:
 
-    1 <= jobDifficulty.length <= 300
-    0 <= jobDifficulty[i] <= 1000
-    1 <= d <= 10
+* 1 <= jobDifficulty.length <= 300
+* 0 <= jobDifficulty[i] <= 1000
+* 1 <= d <= 10
 
 */
 
 class Solution {
 public:
-
+/*
     vector<vector<int>> dp;
     int minFrom(int offset, vector<int>& jobDifficulty, int d) {
         cout << "minFrom " << offset << " " << d ;
@@ -98,6 +94,48 @@ public:
                 max_day + minFrom(i+1, jobDifficulty, d-1)
             );
         }
-        return minD; 
+        return minD;
+    }
+*/
+
+    vector<vector<int>> dp;
+    int minDifficultyFrom(int offset, vector<int>& jobDifficulty, int d) {
+        if (d == 0) { return 0; }
+        if (dp[d][offset] != -2) { return dp[d][offset]; }
+
+        if (d == 1) {
+            dp[d][offset] = *max_element(jobDifficulty.begin()+offset, jobDifficulty.end());
+            return dp[d][offset];
+        }
+        if (jobDifficulty.size() - offset < d) { return -1; }
+        if (d == jobDifficulty.size() - offset) {
+            dp[d][offset] = jobDifficulty[offset] + minDifficultyFrom(offset+1,  jobDifficulty, d-1);
+            return dp[d][offset];
+        }
+
+        int maxToday = jobDifficulty[offset];
+        int minTotal = -1;
+        for (int i=offset; i<=jobDifficulty.size()-d; ++i) {
+            if (maxToday < jobDifficulty[i]) {
+                maxToday = jobDifficulty[i];
+            }
+            int newMin = minDifficultyFrom(i+1,  jobDifficulty, d-1);
+            if (newMin == -1) { break; }
+            if (minTotal == -1) {
+                minTotal = newMin + maxToday;
+            } else {
+                minTotal = min(
+                    minTotal,
+                    newMin + maxToday
+                );
+            }
+        }
+        dp[d][offset] = minTotal;
+        return minTotal;
+    }
+
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        dp = vector(d+1, vector(jobDifficulty.size()+1, -2));
+        return minDifficultyFrom(0, jobDifficulty, d);
     }
 };
