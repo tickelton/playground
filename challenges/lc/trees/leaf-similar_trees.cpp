553aa1470f2f444436183e471209a372c0ca707e
tickelton <tickelton@gmail.com>
2024-01-09 12:43:09 +0100

    Adds lc:leaf-similar_trees

---
+++ challenges/lc/trees/leaf-similar_trees.cpp
@@ -0,0 +1,66 @@
+/*
+
+# Leaf-Similar Trees
+
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+
+## Example 1:
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+
+
+## Example 2:
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+
+
+## Constraints:
+
+* The number of nodes in each tree will be in the range [1, 200].
+* Both of the given trees will have values in the range [0, 200].
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void getLeafSequence(TreeNode* root, vector<int>& s) {
+        if (!root->left && !root->right) {
+            s.push_back(root->val);
+        }
+        if (root->left) {
+            getLeafSequence(root->left, s);
+        }
+        if (root->right) {
+            getLeafSequence(root->right, s);
+        }
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> seq_l;
+        getLeafSequence(root1, seq_l);
+        vector<int> seq_r;
+        getLeafSequence(root2, seq_r);
+
+        return seq_l == seq_r;
+    }
+};
