9f0c1d08ab678bdcd356fa6f52825bd717ea33d4
tickelton <tickelton@gmail.com>
2024-01-13 17:50:03 +0100

    Adds lc:amount_of_time_for_binary_tree_to_be_infected

--- challenges/lc/trees/amount_of_time_for_binary_tree_to_be_infected.cpp
+++ challenges/lc/trees/amount_of_time_for_binary_tree_to_be_infected.cpp
@@ -40,3 +40,64 @@ Explanation: At minute 0, the only node in the tree is infected so we return 0.
 * A node with a value of start exists in the tree.
 
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, vector<int>> graph;
+
+    void buildGraph(TreeNode* r) {
+        if (!r) {
+            return;
+        }
+        if (r->left) {
+            graph[r->val].push_back(r->left->val);
+            graph[r->left->val].push_back(r->val);
+            buildGraph(r->left);
+        }
+        if (r->right) {
+            graph[r->val].push_back(r->right->val);
+            graph[r->right->val].push_back(r->val);
+            buildGraph(r->right);
+        }
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        buildGraph(root);
+
+        queue<int> q;
+        int distance = -1;
+        unordered_set<int> visited;
+        q.push(start);
+        visited.insert(start);
+
+        while (!q.empty()) {
+            int levelNodes = q.size();
+            while (levelNodes > 0) {
+                int cur = q.front();
+                q.pop();
+
+                for (int i: graph[cur]) {
+                    if (!visited.count(i)) {
+                        visited.insert(i);
+                        q.push(i);
+                    }
+                }
+                --levelNodes;
+            }
+            ++distance;
+        }
+
+        return distance;
+    }
+};
